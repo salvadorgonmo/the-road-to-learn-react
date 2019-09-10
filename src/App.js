233e158461b621d71ea318a337d2b7e67e37da5f
salvador.gonzalez <salvador.gonzalez@unosquare.com>
2019-09-10 18:05:34 -0500

    using function inside of setState to avoid having future issues with stale state

--- src/App.js
+++ src/App.js
@@ -22,6 +22,29 @@ const SORTS = {
   POINTS: list => sortBy(list, 'points').reverse(),
 }
 
+const updateSearchTopStoriesState = (hits, page) => {
+  return (prevState) => {
+    {
+      const { searchKey, results } = prevState
+      const oldHits = results && results[searchKey]
+      ? results[searchKey].hits
+      : []
+      const updatedHits = [
+        ...oldHits,
+        ...hits
+      ]
+
+      return {
+        results: {
+          ...results,
+          [searchKey]: { hits: updatedHits, page }
+        },
+        isLoading: false
+      }
+    }
+  }
+}
+
 class App extends Component {
   _isMounted = false
 
@@ -38,23 +61,7 @@ class App extends Component {
 
   setSearchTopStories = (result) => {
     const { hits, page } = result
-    const { searchKey, results } = this.state
-
-    const oldHits = results && results[searchKey]
-      ? results[searchKey].hits
-      : []
-    const updatedHits = [
-      ...oldHits,
-      ...hits
-    ]
-
-    this.setState({
-      results: {
-        ...results,
-        [searchKey]: { hits: updatedHits, page }
-      },
-      isLoading: false
-    })
+    this.setState(updateSearchTopStoriesState(hits, page))
   }
 
   onDismiss = id => {
